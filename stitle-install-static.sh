cb69f0e2463b9bd719f85b1d81dac8d3a2935b13
martin jakubik <martin.jakubik@sap.com>
2024-03-17 10:24:33 +0100

    fix: files were not moved if directory didnt exist

--- stitle-install-static.sh
+++ stitle-install-static.sh
@@ -3,32 +3,32 @@
 # installs planets in content/planets directory
 if [[ -d /home/martin/stitlestaticsiteupload/content/planets ]] ; then
   rm -r /var/www/www.supertitle.org/htdocs/content/planets
-  mv /home/martin/stitlestaticsiteupload/content/planets /var/www/www.supertitle.org/htdocs/content/
 fi
+mv /home/martin/stitlestaticsiteupload/content/planets /var/www/www.supertitle.org/htdocs/content/
 
 # installs fractals in content/fractals directory
 if [[ -d /home/martin/stitlestaticsiteupload/content/fractals ]] ; then
   rm -r /var/www/www.supertitle.org/htdocs/content/fractals
-  mv /home/martin/stitlestaticsiteupload/content/fractals /var/www/www.supertitle.org/htdocs/content/
 fi
+mv /home/martin/stitlestaticsiteupload/content/fractals /var/www/www.supertitle.org/htdocs/content/
 
 # installs blaufisch in content/blaufisch directory
 if [[ -d /home/martin/stitlestaticsiteupload/content/blaufisch ]] ; then
   rm -r /var/www/www.supertitle.org/htdocs/content/blaufisch
-  mv /home/martin/stitlestaticsiteupload/content/blaufisch /var/www/www.supertitle.org/htdocs/content/
 fi
+mv /home/martin/stitlestaticsiteupload/content/blaufisch /var/www/www.supertitle.org/htdocs/content/
 
 # installs knights in content/knights directory
 if [[ -d /home/martin/stitlestaticsiteupload/content/knights ]] ; then
   rm -r /var/www/www.supertitle.org/htdocs/content/knights
-  mv /home/martin/stitlestaticsiteupload/content/knights /var/www/www.supertitle.org/htdocs/content/
 fi
+mv /home/martin/stitlestaticsiteupload/content/knights /var/www/www.supertitle.org/htdocs/content/
 
 # installs server
 if [[ -d /var/www/www.supertitle.org/server ]] ; then
   rm -r /var/www/www.supertitle.org/server
-  mv /home/martin/stitlestaticsiteupload/server /var/www/www.supertitle.org/
 fi
+mv /home/martin/stitlestaticsiteupload/server /var/www/www.supertitle.org/
 
 mv /home/martin/stitlestaticsiteupload/package.json /var/www/www.supertitle.org/
 starting_directory=$PWD
