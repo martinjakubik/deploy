10c6aaa33d3935b41c53a542766a62aa06cd7eca
martin jakubik <martin.jakubik@sap.com>
2024-04-21 10:40:22 +0200

    allowed uploading files listed in another file

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # sets up usage
-USAGE="usage: $0 -i --inputDir inputDir -s siteId --siteShortName siteShortName -u --userId userId --ip ipAddress --debug"
+USAGE="usage: $0 -i --inputDir inputDir -s siteId --siteShortName siteShortName -u --userId userId --ip ipAddress -c --incremental -d --debug"
 
 # set up defaults
 DEBUG=0
@@ -8,6 +8,7 @@ inputDir=~/inputDir
 siteId=abcd
 siteShortName=abcdhome
 destinationDir=~/destinationDir
+incremental=0
 
 # parses and reads command line arguments
 while [ $# -gt 0 ]
@@ -20,6 +21,8 @@ do
     (-u) userId="$2"; shift;;
     (--userId) userId="$2"; shift;;
     (--ip) ipAddress="$2"; shift;;
+    (-c) incremental=1;;
+    (--incremental) incremental=1;;
     (-d) DEBUG=1;;
     (--debug) DEBUG=1;;
     (-*) echo >&2 ${USAGE}
@@ -40,6 +43,29 @@ echo   "site nick     : $siteShortName"
 echo   "user          : $userId"
 echo   "IP address    : $ipAddress"
 
+upload_listed_files() {
+  echo "uploading files listed in upload_files.txt"
+  if [[ -e upload_files.txt ]] ; then
+    file_array=()
+    while IFS= read -r line; do
+      file_array+=($line)
+    done < upload_files.txt
+    for filename in "${file_array[@]}" ; do
+      requested_filename=${HTROOT_SOURCE_DIR}/content/$filename
+      if [[ -e $requested_filename ]] ; then
+        if [[ $DEBUG -eq 0 ]] ; then
+          scp $requested_filename $DESTINATION_DIR
+        else
+          echo uploading $requested_filename
+        fi
+      else
+        echo the file: $requested_filename does not exist
+      fi
+    done
+  else
+    echo the list of files upload_files.txt does not exist
+  fi
+}
 
 if [[ $DEBUG -eq 0 ]] ; then
     ./prepare.sh --inputDir ${inputDir} -s ${siteId} --siteShortName ${siteShortName}
@@ -60,7 +86,11 @@ else
     echo scp -r ${inputDir}/server $DESTINATION_DIR
     echo scp ${inputDir}/package.json $DESTINATION_DIR
 
-    echo scp -r ${HTROOT_SOURCE_DIR}/content $DESTINATION_DIR
+    if [[ ${incremental} -eq 0 ]] ; then
+      echo scp -r ${HTROOT_SOURCE_DIR}/content $DESTINATION_DIR
+    elif [[ ${incremental} -eq 1 ]] ; then
+      upload_listed_files
+    fi
 
     echo scp ${HTROOT_SOURCE_DIR}/index.html ${HTROOT_SOURCE_DIR}/screen.css ${DESTINATION_DIR}
 
