15b22c0048f1aacf396dac5d18cb1cc26577ddf3
martin jakubik <martin.jakubik@sap.com>
2024-04-27 19:52:52 +0200

    basic incremental install  works

--- stitle-install-static.sh
+++ stitle-install-static.sh
@@ -8,66 +8,66 @@ incremental=0
 # parses and reads command line arguments
 while [ $# -gt 0 ]
 do
-  case "$1" in
-    (-c) incremental=1;;
-    (--incremental) incremental=1;;
-    (-*) echo >&2 ${USAGE}
-    exit 1;;
-  esac
-  shift
+	case "$1" in
+		(-c) incremental=1;;
+		(--incremental) incremental=1;;
+		(-*) echo >&2 ${USAGE}
+		exit 1;;
+	esac
+		shift
 done
 
 incremental_install_content() {
 	# installs planets in content/planets directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/planets ]] ; then
-		mv /home/martin/stitlestaticsiteupload/content/planets /var/www/www.supertitle.org/htdocs/content/
+		mv /home/martin/stitlestaticsiteupload/content/planets/app/* /var/www/www.supertitle.org/htdocs/content/planets/app/
 	fi
 
 	# installs fractals in content/fractals directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/fractals ]] ; then
-	  mv /home/martin/stitlestaticsiteupload/content/fractals /var/www/www.supertitle.org/htdocs/content/
+		mv /home/martin/stitlestaticsiteupload/content/fractals/app/* /var/www/www.supertitle.org/htdocs/content/fractals/app/
 	fi
 
 	# installs blaufisch in content/blaufisch directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/blaufisch ]] ; then
-	  mv /home/martin/stitlestaticsiteupload/content/blaufisch /var/www/www.supertitle.org/htdocs/content/
+		mv /home/martin/stitlestaticsiteupload/content/blaufisch/app/* /var/www/www.supertitle.org/htdocs/content/blaufisch/app/
 	fi
 
 	# installs knights in content/knights directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/knights ]] ; then
-	  mv /home/martin/stitlestaticsiteupload/content/knights/* /var/www/www.supertitle.org/htdocs/content/knights/
+		mv /home/martin/stitlestaticsiteupload/content/knights/app/* /var/www/www.supertitle.org/htdocs/content/knights/app/
 	fi
 }
 
 clean_install_content() {
 	# installs planets in content/planets directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/planets ]] ; then
-	  rm -r /var/www/www.supertitle.org/htdocs/content/planets
+		rm -r /var/www/www.supertitle.org/htdocs/content/planets
 	fi
 	mv /home/martin/stitlestaticsiteupload/content/planets /var/www/www.supertitle.org/htdocs/content/
 
 	# installs fractals in content/fractals directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/fractals ]] ; then
-	  rm -r /var/www/www.supertitle.org/htdocs/content/fractals
+		rm -r /var/www/www.supertitle.org/htdocs/content/fractals
 	fi
 	mv /home/martin/stitlestaticsiteupload/content/fractals /var/www/www.supertitle.org/htdocs/content/
 
 	# installs blaufisch in content/blaufisch directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/blaufisch ]] ; then
-	  rm -r /var/www/www.supertitle.org/htdocs/content/blaufisch
+		rm -r /var/www/www.supertitle.org/htdocs/content/blaufisch
 	fi
 	mv /home/martin/stitlestaticsiteupload/content/blaufisch /var/www/www.supertitle.org/htdocs/content/
 
 	# installs knights in content/knights directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/knights ]] ; then
-	  rm -r /var/www/www.supertitle.org/htdocs/content/knights
+		rm -r /var/www/www.supertitle.org/htdocs/content/knights
 	fi
 	mv /home/martin/stitlestaticsiteupload/content/knights /var/www/www.supertitle.org/htdocs/content/
 }
 
 # installs server
 if [[ -d /var/www/www.supertitle.org/server ]] ; then
-  rm -r /var/www/www.supertitle.org/server
+	rm -r /var/www/www.supertitle.org/server
 fi
 mv /home/martin/stitlestaticsiteupload/server /var/www/www.supertitle.org/
 chmod a+x /var/www/www.supertitle.org/server/index.js
@@ -86,9 +86,8 @@ mv /home/martin/stitlestaticsiteupload/background.png /var/www/www.supertitle.or
 mv /home/martin/stitlestaticsiteupload/settings.png /var/www/www.supertitle.org/htdocs/
 
 if [[ $incremental -eq 0 ]] ; then
-  clean_install_content
+	clean_install_content
+	rm /home/martin/stitlestaticsiteupload/all_files_uploaded
 elif [[ $incremental -eq 1 ]] ; then
-  incremental_install_content
+	incremental_install_content
 fi
-
-rm /home/martin/stitlestaticsiteupload/all_files_uploaded
