835e244821c2b46a2094ece3fdf6a065701acad4
martin jakubik <martin.jakubik@gmail.com>
2024-11-24 20:16:25 +0100

    tidy

--- stitle-install-static.sh
+++ stitle-install-static.sh
@@ -61,31 +61,31 @@ incremental_install_content() {
 }
 
 clean_install_content() {
-	# installs planets in content/planets directory
+	# cleans content/planets directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/planets ]] ; then
 		rm -r /var/www/www.supertitle.org/htdocs/content/planets
 	fi
 	mv /home/martin/stitlestaticsiteupload/content/planets /var/www/www.supertitle.org/htdocs/content/
 
-	# installs fractals in content/fractals directory
+	# cleans content/fractals directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/fractals ]] ; then
 		rm -r /var/www/www.supertitle.org/htdocs/content/fractals
 	fi
 	mv /home/martin/stitlestaticsiteupload/content/fractals /var/www/www.supertitle.org/htdocs/content/
 
-	# installs blaufisch in content/blaufisch directory
+	# cleans content/blaufisch directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/blaufisch ]] ; then
 		rm -r /var/www/www.supertitle.org/htdocs/content/blaufisch
 	fi
 	mv /home/martin/stitlestaticsiteupload/content/blaufisch /var/www/www.supertitle.org/htdocs/content/
 
-	# installs knights in content/knights directory
+	# cleans content/knights directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/knights ]] ; then
 		rm -r /var/www/www.supertitle.org/htdocs/content/knights
 	fi
 	mv /home/martin/stitlestaticsiteupload/content/knights /var/www/www.supertitle.org/htdocs/content/
 
-	# installs war in content/war directory
+	# cleans content/war directory
 	if [[ -d /home/martin/stitlestaticsiteupload/content/war ]] ; then
 		rm -r /var/www/www.supertitle.org/htdocs/content/war
 	fi
