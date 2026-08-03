3ae6b36fd4296ff1fe105038eec6558d3295dc9e
Martin JAKUBIK <martin.jakubik@sap.com>
2026-08-03 11:46:24 +0200

    added build picket script

---
+++ build-picket.sh
@@ -0,0 +1,9 @@
+#!/usr/local/bin/fish
+
+set source_files ~/code/gitwork/deploy/picket.sh ~/code/gitwork/deploy/picket-deploy-site.sh
+set source_dirname $(dirname $source_files[1])
+
+for source_file in $source_files
+    set destination_file $(basename $(path change-extension '' $source_file))
+    cp $source_file $source_dirname/build/$destination_file
+end
