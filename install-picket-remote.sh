7d979561e18963673de9a7722e6ef295ccee97e6
Martin JAKUBIK <martin.jakubik@sap.com>
2026-08-03 12:36:36 +0200

    added picket remote install script

---
+++ install-picket-remote.sh
@@ -0,0 +1,14 @@
+#!/usr/local/bin/fish
+
+set local_files_to_install ~/code/gitwork/deploy/build/picket ~/code/gitwork/deploy/build/picket-deploy-site
+set files_to_install_local_dirname $(dirname $local_files_to_install[1])
+
+for local_file_to_install in $local_files_to_install
+    if test -f $local_file_to_install
+        set file_to_install_basename $(basename $local_file_to_install)
+        scp $local_file_to_install martin@192.46.222.142:~/
+        ssh -t martin@192.46.222.142 'sudo mv ~/'$file_to_install_basename' /usr/local/bin/'$file_to_install_basename' ; sudo chmod a+x /usr/local/bin/'$file_to_install_basename' ; sudo chown root:root /usr/local/bin/'$file_to_install_basename
+    end
+end
+
+ssh martin@192.46.222.142 'ls -la /usr/local/bin'
