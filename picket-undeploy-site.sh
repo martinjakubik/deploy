5b40aec8daee87568bcb3e49c9058dacc6563260
Martin JAKUBIK <martin.jakubik@sap.com>
2026-08-04 12:17:54 +0200

    added skeleton delete and undeploy site commands

---
+++ picket-undeploy-site.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# sets up usage
+USAGE="usage: $0 -s|--siteId siteId"
+
+# parses and reads command line arguments
+while [ $# -gt 0 ]
+do
+	case "$1" in
+		(-s) siteId="$2"; shift;;
+        (--siteId) siteId="$2"; shift;;
+		(-*) echo >&2 ${USAGE}
+		exit 1;;
+	esac
+		shift
+done
+
+echo undeploying site \"${siteId}\"
+exit 0
