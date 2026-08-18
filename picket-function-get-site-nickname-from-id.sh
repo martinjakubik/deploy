4b60aa2b5cc465de46cb8a06990dd2ed1329326e
martin jakubik <martin.jakubik@gmail.com>
2026-08-18 16:24:17 +0200

    added some site ids and nicknames

--- picket-function-get-site-nickname-from-id.sh
+++ picket-function-get-site-nickname-from-id.sh
@@ -1,8 +1,10 @@
 siteId="$1"
 
 case "$siteId" in
+    (abcd) echo -n "abcdhome" ;;
     (stitle) echo -n "supertitle" ;;
     (fiza) echo -n "fizasport" ;;
+    (guppy) echo -n "superguppy" ;;
     (*) echo
     exit 1;;
 esac
--- picket-function-get-site-project-root-from-id.sh
+++ picket-function-get-site-project-root-from-id.sh
@@ -1,8 +1,10 @@
 siteId="$1"
 
 case "$siteId" in
+    (abcd) echo -n "www.abcdhome.name" ;;
     (stitle) echo -n "www.supertitle.org" ;;
-    (fiza) echo -n "www.fizasport.org" ;;
+    (fiza) echo -n "www.fizasport.com" ;;
+    (guppy) echo -n "www.superguppy.io" ;;
     (*) echo
     exit 1;;
 esac
